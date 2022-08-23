78690b1dbd1a00ea8caa6f94f713cfceeab0d0ba
Bulaien <brianjones71@googlemail.com>
2022-08-23 20:03:08 +0200

    add "arbeit"-section

--- app.js
+++ app.js
@@ -33,3 +33,14 @@ close.addEventListener('click', openModalUber)
 function openModalUber() {
     modalUber.style.display === "none" ? modalUber.style.display = 'block' : modalUber.style.display = 'none';
 }
+
+
+let modalArbeit = document.querySelector('.modal-arbeiten')
+let arbeit = document.querySelector('.arbeit')
+arbeit.addEventListener('click', openModalArbeit)
+let close1 = document.querySelector('.window-right1')
+close1.addEventListener('click', openModalArbeit)
+function openModalArbeit() {
+    modalArbeit.style.display === "none" ? modalArbeit.style.display = 'block' : modalArbeit.style.display = 'none';
+}
+
